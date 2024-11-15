bc36f6e1508052ba5bce9657fe9c02834b27e456
douglas321 <sperrycarter@gmail.com>
2024-11-15 14:34:53 -0700

    Added HTML elements and basic CSS

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+TODO:
+GameBoard:
+    clicking on a square places an X or O
+    Checks gamestate for winner(maybe put in gamestatus)
+GameStatus:
+    Displays a winner
+ResetButton:
+
+COMPLETED:
+GameBoard:
+
+GameStatus:
+
+ResetButton:
+    resets game logic/board state. Mayeb just reload page? 
+*/
+
+function reloadPage() {
+    location.reload();
+}
